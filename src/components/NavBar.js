f6c3a839fb1f50825c1e3153b2b7d5961ef583d7
Red Fairy <agorelik2@gmail.com>
2020-10-30 14:00:18 -0400

    fixed NavBar component to display links correctly

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,68 +3,96 @@ import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
-    <div className="container-nav">
-      <ul className="nav nav-tabs">
-        <li className="nav-item">
+    <nav className="navbar navbar-expand-md navbar-light nav-style">
+      <div className="row justify-content-center">
+        <div className="col-6 d-flex justify-content-center">
           <Link
             to="/"
             className={
               window.location.pathname === "/" ? "nav-link active" : "nav-link"
             }
           >
-            Alina Gorelik
+            <h2 className="text-custom">Alina Gorelik</h2>
           </Link>
-        </li>
-        <li className="nav-item">
-          <Link
-            to="/"
-            className={
-              window.location.pathname === "/about"
-                ? "nav-link active"
-                : "nav-link"
-            }
+          {/* <a className="navbar-brand" href="index.html">
+            <h2 className="text-custom">Alina Gorelik</h2>
+          </a> */}
+        </div>
+        <div className="col-6 d-flex justify-content-center">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
           >
-            About
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link
-            to="/Portfolio"
-            className={
-              window.location.pathname === "/Portfolio"
-                ? "nav-link active"
-                : "nav-link"
-            }
-          >
-            Portfolio
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link
-            to="/contact"
-            className={
-              window.location.pathname === "/contact"
-                ? "nav-link active"
-                : "nav-link"
-            }
-          >
-            Contact
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link
-            to="/contact/learn"
-            className={
-              window.location.pathname === "/resume"
-                ? "nav-link active"
-                : "nav-link"
-            }
-          >
-            Resume
-          </Link>
-        </li>
-      </ul>
-    </div>
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ml-auto justify-content-end">
+              <li className="nav-item active portfolio-link">
+                <Link
+                  to="/"
+                  className={
+                    window.location.pathname === "/about"
+                      ? "nav-link active"
+                      : "nav-link"
+                  }
+                >
+                  About
+                </Link>
+                {/* <a className="nav-link portfolio-link" href="index.html">
+                  About
+                </a> */}
+              </li>
+              <li className="nav-item portfolio-link">
+                <Link
+                  to="/Portfolio"
+                  className={
+                    window.location.pathname === "/Portfolio"
+                      ? "nav-link active"
+                      : "nav-link"
+                  }
+                >
+                  Portfolio
+                </Link>
+                {/* <a
+                  className="nav-link portfolio-link"
+                  href="new_portfolio.html"
+                >
+                  Portfolio
+                </a> */}
+              </li>
+              <li className="nav-item">
+                {/* <a
+                  className="nav-link portfolio-link"
+                  href="assets/Alina_Gorelik_Resume.pdf"
+                >
+                  Resume
+                </a> */}
+              </li>
+              <li className="nav-item portfolio-link">
+                <Link
+                  to="/contact"
+                  className={
+                    window.location.pathname === "/contact"
+                      ? "nav-link active"
+                      : "nav-link"
+                  }
+                >
+                  Contact
+                </Link>
+                {/* <a className="nav-link" href="contact.html">
+                  Contact
+                </a> */}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
   );
 }
 
