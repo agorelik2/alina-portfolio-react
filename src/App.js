270738b2464bb5f55e5883ec720eff3e25fcc261
Red Fairy <agorelik2@gmail.com>
2020-10-30 17:16:20 -0400

    re-building portfolio page using .map

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
@@ -9,23 +9,38 @@ import About from "./components/About";
 import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
 import SocMedia from "./components/SocMedia";
+import projects from "./utils/projects.json";
 
-function App() {
-  return (
-    <Router>
-      <NavBar />
-      <div className="container container-fluid">
-        {/* <Route exact path="/" component={Home} /> */}
-        <Route exact path="/" component={About} />
-        <Route exact path="/portfolio" component={Portfolio} />
-        <Route path="/contact" component={Contact} />
-        <SocMedia />
-        <br />
-        <br />
-        <Footer />
-      </div>
-    </Router>
-  );
+class App extends Component {
+  state = {
+    projects: [],
+  };
+
+  componentDidMount() {
+    this.setState({
+      projects: projects,
+    });
+  }
+  // state = { projects };
+  render() {
+    return (
+      <Router>
+        <NavBar />
+        <div className="container container-fluid">
+          <Route exact path="/" component={About} />
+          {/* <Route exact path="/portfolio" component={Portfolio} /> */}
+          <Route
+            path="/projects"
+            render={() => <Portfolio projects={this.state.projects} />}
+          />
+          <Route path="/contact" component={Contact} />
+          <SocMedia />
+          <br />
+          <br />
+          <Footer />
+        </div>
+      </Router>
+    );
+  }
 }
-
 export default App;
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Header from "./Header";
 import Project from "./Project";
-import projects from "../utils/projects.json";
+// import projects from "../utils/projects.json";
 
-function Portfolio() {
+function Portfolio(props) {
+  console.log(props);
   return (
     <div className="portfolio-container">
       <div className="m-5 pt-5 pb-5">
@@ -11,50 +12,18 @@ function Portfolio() {
           <Header title="My Projects" />
         </div>
         <div className="row">
-          <Project
-            name={projects[0].name}
-            image={projects[0].image}
-            project={projects[0].project}
-            location={projects[0].location}
-            github={projects[0].github}
-          />
-          <Project
-            name={projects[1].name}
-            image={projects[1].image}
-            project={projects[1].project}
-            location={projects[1].location}
-            github={projects[1].github}
-          />
-          <Project
-            name={projects[2].name}
-            image={projects[2].image}
-            project={projects[2].project}
-            location={projects[2].location}
-            github={projects[2].github}
-          />
-        </div>
-        <div className="row">
-          <Project
-            name={projects[3].name}
-            image={projects[3].image}
-            project={projects[3].project}
-            location={projects[3].location}
-            github={projects[3].github}
-          />
-          <Project
-            name={projects[4].name}
-            image={projects[4].image}
-            project={projects[4].project}
-            location={projects[4].location}
-            github={projects[4].github}
-          />
-          <Project
-            name={projects[5].name}
-            image={projects[5].image}
-            project={projects[5].project}
-            location={projects[5].location}
-            github={projects[5].github}
-          />
+          {props.projects.map((project, index) => {
+            return (
+              <Project
+                key={index}
+                name={project.name}
+                image={project.image}
+                project={project.project}
+                location={project.location}
+                github={project.github}
+              />
+            );
+          })}
         </div>
       </div>
     </div>
