825b87d0e1d75c1da0128cad0392be61b4128928
Red Fairy <agorelik2@gmail.com>
2020-11-02 23:24:51 -0500

    Updates

---
+++ precache-manifest.ac9c302988c7768f253ded2a9cad67c9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3210dc89c2d4ab584f2e73a9da694b6",
+    "url": "/alina-portfolio-react/index.html"
+  },
+  {
+    "revision": "7e875cb84483b19c1c4f",
+    "url": "/alina-portfolio-react/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "f5d86fa84f6eb4cd32d6",
+    "url": "/alina-portfolio-react/static/css/main.3d0b925a.chunk.css"
+  },
+  {
+    "revision": "7e875cb84483b19c1c4f",
+    "url": "/alina-portfolio-react/static/js/2.94cabedd.chunk.js"
+  },
+  {
+    "revision": "922a8bb592cca1a054db",
+    "url": "/alina-portfolio-react/static/js/3.3154355f.chunk.js"
+  },
+  {
+    "revision": "f5d86fa84f6eb4cd32d6",
+    "url": "/alina-portfolio-react/static/js/main.ef2ffb62.chunk.js"
+  },
+  {
+    "revision": "7966df488e694b7e789c",
+    "url": "/alina-portfolio-react/static/js/runtime~main.f9da3911.js"
+  },
+  {
+    "revision": "4c3fe56fcc538a4881222c9080d0cb5d",
+    "url": "/alina-portfolio-react/static/media/Alina_Gorelik_Resume.4c3fe56f.pdf"
+  },
+  {
+    "revision": "ae6566b4afaecfa81deab597ec5fc084",
+    "url": "/alina-portfolio-react/static/media/Alina_Photo.ae6566b4.jpg"
+  },
+  {
+    "revision": "d0cebdb179e78c6820f2032b8c703193",
+    "url": "/alina-portfolio-react/static/media/New_Alina_Resume.d0cebdb1.pdf"
+  }
+]);
