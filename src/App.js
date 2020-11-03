1cb025e7995eb5bd78ab56a87bd442c19261fa6f
Red Fairy <agorelik2@gmail.com>
2020-11-02 23:20:05 -0500

    changed router to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Link } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import About from "./components/About";
@@ -23,29 +24,29 @@ class App extends Component {
 
   render() {
     return (
-      <Router basename="/alina-portfolio-react">
+      <HashRouter basename="/">
         <NavBar />
         <div className="container container-fluid">
-          <Switch>
-            {/*<Route path="/alina-portfolio-react/" component={About} /> */}
-            <Route exact path="/">
-              <About />
-            </Route>
-            <Route path="/about" component={About} />
-            {/* <Route component={About} /> */}
-            <Route
-              path="/portfolio"
-              render={() => <Portfolio projects={this.state.projects} />}
-            />
-            <Route path="/resume" component={Resume} />
-            <Route path="/contact" component={Contact} />
-            {/*<Route>
+          {/* <Switch> */}
+          {/*<Route path="/alina-portfolio-react/" component={About} /> */}
+          <Route exact path="/">
+            <About />
+          </Route>
+          <Route path="/about" component={About} />
+          {/* <Route component={About} /> */}
+          <Route
+            path="/portfolio"
+            render={() => <Portfolio projects={this.state.projects} />}
+          />
+          <Route path="/resume" component={Resume} />
+          <Route path="/contact" component={Contact} />
+          {/*<Route>
               <About />
             </Route>*/}
-          </Switch>
+          {/* </Switch> */}
           <Footer />
         </div>
-      </Router>
+      </HashRouter>
     );
   }
 }
